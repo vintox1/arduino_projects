7337ac4c7cbbf192e7cc7a67209549bc4f8e3bec
Jonathan Ings <jonathanings@Jonathans-MacBook-Air.local>
2020-04-13 16:34:38 +0200

    Update for final board support and multiple sensors

--- ThermistorTempSensor/ThermistorTempSensor.ino
+++ ThermistorTempSensor/ThermistorTempSensor.ino
@@ -4,23 +4,35 @@ Adafruit_SSD1306 display(128, 64);  // Create display
 #include <Fonts/FreeMonoBold12pt7b.h>  // Add a custom font
 #include <Fonts/FreeMono9pt7b.h>  // Add a custom font
 
-int ThermistorPin = 0;
+int ThermistorPin1 = 1;
+int ThermistorPin2 = 2;
+int ThermistorPin3 = 3;
 int Vo;
 float R1 = 10000;
 float logR2, R2, T, Tc, Tf;
 float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
 
-int set = 1;
+int set = 0;
 float vSet;
 float setOut;
 
-int rel = 3;
+int rel1 = 2;
+int rel2 = 3;
+int rel3 = 4;
+int rel4 = 5;
 
 char data = 0;
 
 void setup() {
 Serial.begin(9600);
-pinMode(rel, OUTPUT);
+pinMode(rel1, OUTPUT);
+digitalWrite(rel1, HIGH);
+pinMode(rel2, OUTPUT);
+digitalWrite(rel2, HIGH);
+pinMode(rel3, OUTPUT);
+digitalWrite(rel3, HIGH);
+pinMode(rel4, OUTPUT);
+digitalWrite(rel4, HIGH);
 
   //display setup 
   delay(100);  // This delay is needed to let the display to initialize
@@ -49,42 +61,45 @@ void loop() {
     }
     if(data == '1')            //Checks whether value of data is equal to 1 
       {
-        digitalWrite(rel, HIGH);//switch relay on
+        digitalWrite(rel1, LOW);//switch relay on
         Serial.print("Relay On for 1 Second\n");
-        delay(1000);
-        digitalWrite(rel, LOW);//switch relay off
+        delay(2000);
+        digitalWrite(rel1, HIGH);//switch relay off
         Serial.print("Relay Off Again\n");
         
       }
-        else if(data == '2')       //Checks whether value of data is equal to 0
-        {
-        digitalWrite(rel, HIGH);//switch relay on
+    if(data == '2')       //Checks whether value of data is equal to 0
+      {
+        digitalWrite(rel2, LOW);//switch relay on
         Serial.print("Relay On for 2 Second\n");
         delay(2000);
-        digitalWrite(rel, LOW);//switch relay off
+        digitalWrite(rel2, HIGH);//switch relay off
         Serial.print("Relay Off Again\n");
-        }
+      }
 
-
-//screen loop
-display.clearDisplay();  // Clear the display so we can refresh
-display.setFont(&FreeMono9pt7b);  // Set a custom font
-display.setTextSize(0);  // Set text size. We are using a custom font so you should always use the text size of 0
-
-// Print text:
-display.setCursor(0, 10);  // (x,y)
-display.println("Check Temp");  // Text or value to print
-display.drawLine(0, 15, 120, 15, WHITE);  // Draw line (x0,y0,x1,y1,color)
-display.setCursor(0, 35);
-display.println("Set:");
-display.setCursor(0, 55);
-display.println("Tmp:");
+    if(data == '3')       //Checks whether value of data is equal to 0
+      {
+        digitalWrite(rel3, LOW);//switch relay on
+        Serial.print("Relay On for 2 Second\n");
+        delay(2000);
+        digitalWrite(rel3, HIGH);//switch relay off
+        Serial.print("Relay Off Again\n");
+      }
+    
+    if(data == '4')       //Checks whether value of data is equal to 0
+      {
+        digitalWrite(rel4, LOW);//switch relay on
+        Serial.print("Relay On for 2 Second\n");
+        delay(2000);
+        digitalWrite(rel4, HIGH);//switch relay off
+        Serial.print("Relay Off Again\n");
+      }
 
 
   vSet = analogRead(set);
   setOut = (vSet /1023)*100;
 
-  Vo = analogRead(ThermistorPin);
+  Vo = analogRead(ThermistorPin1);
   R2 = R1 * (1023.0 / (float)Vo - 1.0);
   logR2 = log(R2);
   T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
@@ -92,10 +107,56 @@ display.println("Tmp:");
   Tf = (Tc * 9.0)/ 5.0 + 32.0; 
 
 
+//Start of screen loop - - - - - - - 
+display.clearDisplay();  // Clear the display so we can refresh
+display.setFont(&FreeMono9pt7b);  // Set a custom font
+display.setTextSize(0);  // Set text size. We are using a custom font so you should always use the text size of 0
 
+//display setting value
+display.setCursor(0, 11);
+display.println("Set :");
+display.setCursor(60, 11);
+display.println(setOut); 
+//Display Tmp1 value
+display.setCursor(0, 26);
+display.println("Tmp1:");
+display.setCursor(60, 26);
+display.println(Tc); 
+//Display Tmp2 value
+display.setCursor(0, 41);
+display.println("Tmp2:");
+display.setCursor(60, 41);
+display.println(Tc); 
+
+//Display Tmp3 value
+display.setCursor(0, 56);
+display.println("Tmp3:");
+display.setCursor(60, 56);
+display.println(Tc); 
+
+// Print everything we set previously
+display.display();  
+
+
+//Serial Print Set value 
+Serial.print("Set: ");
+Serial.print(setOut);
+Serial.print("\n");
+//Serial print Tmp Values 
+Serial.print("Tmp1: ");
+Serial.print(Tc);
+Serial.print("\n");
+Serial.print("Tmp2: ");
+Serial.print(Tc);
+Serial.print("\n");
+Serial.print("Tmp3: ");
+Serial.print(Tc);
+Serial.print("\n");
+
+//Check if Tmp exceeds set value and turn on water is required 
     if (Tc > setOut){
 
-      digitalWrite(rel, HIGH);//switch relay on 
+      digitalWrite(rel1, LOW);//switch relay on 
       Serial.print("Cooling on 3 seconds \n");
 
       //clear and reset display
@@ -105,13 +166,13 @@ display.println("Tmp:");
 
       display.setCursor(0, 35);
       display.println("-- Cooling");
-      display.setCursor(0, 55);
-      display.println("-- Cooling");
+      display.display();  // Print everything we set previously
+
       Serial.print("Cooling\n");
       
-
-      display.display();  // Print everything we set previously
       delay(3000);
+      //switch cooling water off rel1
+      digitalWrite(rel1, HIGH);//switch relay off 
 
       //clear and reset display
       display.clearDisplay();  // Clear the display so we can refresh
@@ -120,47 +181,14 @@ display.println("Tmp:");
 
       display.setCursor(0, 35);
       display.println("-- Hold");
-      display.setCursor(0, 55);
-      display.println("-- Hold");
-      Serial.print("Holding\n");
-
       display.display();  // Print everything we set previously
+
+      Serial.print("Holding\n");
+      Serial.print("Delay then check again! \n");
       
-      Serial.print("Delay 3 seconds then check again! \n");
-      digitalWrite(rel, LOW);//switch relay off 
       delay(3000);
-
-      //clear and reset display
-      display.clearDisplay();  // Clear the display so we can refresh
-      display.setFont(&FreeMono9pt7b);  // Set a custom font
-      display.setTextSize(0);  // Set text size. We are using a custom font so you should always use the text size of 0
-
       
     }
-
-
-
-display.setCursor(0, 35);
-display.println("Set:");
-display.setCursor(45, 35);
-display.println(setOut); 
-Serial.print("Set: ");
-Serial.print(setOut);
-Serial.print("\n");
-
-display.setCursor(0, 55);
-display.println("Tmp:");
-display.setCursor(45, 55);
-display.println(Tc); 
-Serial.print("Temp: ");
-Serial.print(Tc);
-Serial.print("\n");
-
-
-display.display();  // Print everything we set previously
-
-
-    
 
   delay(2000);
 }
